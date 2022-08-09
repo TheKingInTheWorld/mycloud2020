31d28fe89d7f46f302677a1533f364832e0b34f8
ccrebecca <ccrebecca@qq.com>
2022-08-09 09:09:34 +0800

    config commit6

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -76,5 +76,10 @@ public class PaymentController {
         return serverPort;
     }
 
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin() {
+        return "hi, i'm paymentzipkin server fall back, welcome to atguigu, o(n_n)o哈哈~";
+    }
+
 }
 
